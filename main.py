918a4d65025c65596315dc7a074b705274941cf6
Арсений Никулин <pudge063@Arsenijs-MacBook-Air.local>
2024-11-04 09:54:53 +0400

    add readme

--- main.py
+++ main.py
@@ -28,22 +28,32 @@ if __name__ == "__main__":
     args = sys.argv
     arg_dict = {"-f": None, "debug": False}
     part_of_arg, tmp_arg = False, None
-    for arg in args:
-        if arg[0] == "-":
-            part_of_arg = True
-            tmp_arg = arg
-        elif part_of_arg:
-            arg_dict[tmp_arg] = arg
-            part_of_arg = False
-        else:
-            arg_dict[arg] = True
     try:
+        for arg in args:
+            if arg[0] == "-":
+                part_of_arg = True
+                if not arg in arg_dict:
+                    raise Exception(f"Undefined arg {arg}.")
+                tmp_arg = arg
+            elif part_of_arg:
+                arg_dict[tmp_arg] = arg
+                part_of_arg = False
+            else:
+                arg_dict[arg] = True
+
         if part_of_arg:
             raise Exception(f"Excepted part of {tmp_arg} arg.")
+
         f = arg_dict["-f"] if arg_dict["-f"] else "test1.txt"
         d = True if arg_dict["debug"] else False
+
         start_compile(f, d)
+
         if d:
             print(arg_dict)
+
     except Exception as e:
         print(e)
+
+    finally:
+        print("Finished.")
